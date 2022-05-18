42f63b980c297542077ced43fa0e131b85ab210a
huynhphat12 <57324260+huynhphat12@users.noreply.github.com>
2022-05-18 16:40:04 +1000

    Fixed menu resizing issues + added description

---
+++ IOSassignment-main-2/Restaurant Booking app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant Booking app
+//
+//  Created by Tomi Piisila on 1/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
