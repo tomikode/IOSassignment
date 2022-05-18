84626710469e26fd1d337bec7565a4a8e0fab1ec
tomikode <tomi.piisila@gmail.com>
2022-05-19 07:26:46 +1000

    tables view

--- Restaurant Booking app/TableController.swift
+++ Restaurant Booking app/TableController.swift
@@ -12,11 +12,13 @@ class TableController: UIViewController {
     var name = ""
     var date = Date()
 
+    @IBOutlet weak var mapView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("next")
         print(name)
         print(date)
+//        mapView.frame.size.height *= 2
     }
 
 
